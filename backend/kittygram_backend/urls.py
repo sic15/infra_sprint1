010395554e187cbce3ea055957b6faa1e961d27e
sic15 <sic15@yandex.ru>
2023-08-20 12:12:02 +0000

    4.1

--- backend/kittygram_backend/urls.py
+++ backend/kittygram_backend/urls.py
@@ -1,8 +1,8 @@
-from rest_framework import routers
 from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework import routers
 
 from cats.views import AchievementViewSet, CatViewSet
 
